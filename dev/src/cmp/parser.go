c8647816fede8747f4ae8b40c9ae524c212e4d8a
DrCodfish <kiranrangoon0@proton.me>
2025-03-20 09:07:19 -0400

    Orgainize
    Organized the file system

--- dev/src/cmp/parser.go
+++ dev/src/cmp/parser.go
@@ -1,13 +1,67 @@
-package cmp
+package compiler
 
-type Parser struct {
-    // Add parser fields here
+import (
+	"fmt"
+	"strconv"
+)
+
+// VariableType represents the type of a variable in Zelio
+type VariableType int
+
+const (
+	IntType VariableType = iota
+	FloatType
+	StringType
+	BoolType
+)
+
+// Variable represents a variable in Zelio
+type Variable struct {
+	Name  string
+	Type  VariableType
+	Value interface{}
 }
 
-func NewParser() *Parser {
-    return &Parser{}
+// DeclareVariable declares a new variable
+func DeclareVariable(name string, varType VariableType, value interface{}) Variable {
+	return Variable{
+		Name:  name,
+		Type:  varType,
+		Value: value,
+	}
 }
 
-func (p *Parser) Parse(tokens []Token) {
-    // Add parsing logic here
+// Println prints a message to the console
+func Println(args ...interface{}) {
+	fmt.Println(args...)
+}
+
+// Add adds two integers
+func Add(a, b int) int {
+	return a + b
+}
+
+// Subtract subtracts two integers
+func Subtract(a, b int) int {
+	return a - b
+}
+
+// Multiply multiplies two integers
+func Multiply(a, b int) int {
+	return a * b
+}
+
+// Divide divides two integers
+func Divide(a, b int) int {
+	return a / b
+}
+
+// Concatenate concatenates two strings
+func Concatenate(a, b string) string {
+	return a + b
+}
+
+// Length returns the length of a string
+func Length(s string) int {
+	return len(s)
 }
